3bb6e8d9750309932e8af5b3c62c95b3d31cb0fa
rossberg <rossberg@users.noreply.github.com>
2021-11-22 10:28:36 +0000

    deploy: 5a8a2d92a45eda76a57219933416ddc91e609d4a

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,9 +1,12 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0',
+    VERSION: '1.1 + multi-memory (Draft 2021-11-22)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
-    SOURCELINK_SUFFIX: '.txt'
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
 };
