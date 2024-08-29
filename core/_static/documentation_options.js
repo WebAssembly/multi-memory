a18e4ec5d402da5fbb737b78fc588eadc7484485
rossberg <rossberg@users.noreply.github.com>
2024-08-29 14:12:56 +0000

    deploy: 737843463d84aae576b2f74281c5d8b216b2fba2

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 + multi-memory (Draft 2024-07-31)',
+    VERSION: '2.0 + multi-memory (Draft 2024-08-29)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
